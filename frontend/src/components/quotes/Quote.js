ad76d87a5d0e8fafdec552ce328ffccc4899954b
A1eksa <80787783+A1eksa@users.noreply.github.com>
2022-02-14 21:18:27 +0100

    quotes

--- frontend/src/components/quotes/Quote.js
+++ frontend/src/components/quotes/Quote.js
@@ -1,60 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { API_URL } from '../../utils/constants';
-import { IconContext } from 'react-icons';
-import { FaRedoAlt } from 'react-icons/fa';
+// import React, { useState, useEffect } from 'react';
+// import { API_URL } from '../../utils/constants';
+// import { IconContext } from 'react-icons';
+// import { FaRedoAlt } from 'react-icons/fa';
 
-import { QuoteWrapper, Name, QuoteText, QuoteButton } from './_QuotesStyles';
+// import { QuoteWrapper, Name, QuoteText, QuoteButton } from './_QuotesStyles';
 
-export const Quote = () => {
-  const [theQuote, setTheQuote] = useState({});
+// export const Quote = () => {
+//   const [theQuote, setTheQuote] = useState({});
 
-  const getNewQuote = () => {
-    window.location.reload();
-  };
+//   const getNewQuote = () => {
+//     window.location.reload();
+//   };
 
-  useEffect(() => {
-    fetch(API_URL('quotes'))
-      .then((res) => res.json())
-      .then((data) => {
-        setTheQuote(data.response);
-        console.log('Is this the quote', data);
-      });
-  }, []);
+//   useEffect(() => {
+//     fetch(API_URL('quotes'))
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setTheQuote(data.response);
+//         console.log('Is this the quote', data);
+//       });
+//   }, []);
 
-  // useEffect =
-  //   (() => {
-  //     getNewQuote();
-  //   },
-  //   []);
-
-  // useEffect = (() => {}, [theQuote]);
-
-  // const getNewQuote = () => {
-  //   fetch(API_URL('quotes'))
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setTheQuote(data.response);
-  //       console.log('Is this the quote', data);
-  //     });
-  // };
-
-  return (
-    <QuoteWrapper>
-      <QuoteText>{theQuote.message}</QuoteText>
-      <Name>/ {theQuote.author}</Name>
-      <IconContext.Provider
-        value={{
-          color: '#444444',
-          className: 'global-class-name',
-          size: '1.125rem',
-          fontWeight: 'bold',
-          style: { verticalAlign: 'middle', marginLeft: '0.05rem' },
-        }}
-      >
-        <QuoteButton onClick={() => getNewQuote()}>
-          <FaRedoAlt />
-        </QuoteButton>
-      </IconContext.Provider>
-    </QuoteWrapper>
-  );
-};
+//   return (
+//     <QuoteWrapper>
+//       <QuoteText>{theQuote.message}</QuoteText>
+//       <Name>/ {theQuote.author}</Name>
+//       <IconContext.Provider
+//         value={{
+//           color: '#444444',
+//           className: 'global-class-name',
+//           size: '1.125rem',
+//           fontWeight: 'bold',
+//           style: { verticalAlign: 'middle', marginLeft: '0.05rem' },
+//         }}
+//       >
+//         <QuoteButton onClick={() => getNewQuote()}>
+//           <FaRedoAlt />
+//         </QuoteButton>
+//       </IconContext.Provider>
+//     </QuoteWrapper>
+//   );
+// };
