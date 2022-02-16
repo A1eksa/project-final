6b3ac36cc90a59e2e619bab0cdf83ba1a78f297d
A1eksa <80787783+A1eksa@users.noreply.github.com>
2022-02-16 17:23:25 +0100

    todo form prevent

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ const thoughts = JSON.parse(
 );
 
 const mongoUrl =
-  process.env.MONGO_URL || 'https://aleksa-jessi-final-project.herokuapp.com';
-//process.env.MONGO_URL || 'mongodb://localhost/project-final';
+  // process.env.MONGO_URL || 'https://aleksa-jessi-final-project.herokuapp.com';
+  process.env.MONGO_URL || 'mongodb://localhost/project-final';
 
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useCreateIndex', true); //added due to deprecation error 26868
